39df0288e8bfbef616fe2a4136d1cfaa53298209
syedasifakhtar91 <147165912+syedasifakhtar91@users.noreply.github.com>
2026-06-04 17:47:14 +0530

    Update emails.py

--- emails.py
+++ emails.py
@@ -75,4 +75,4 @@ if __name__ == "__main__":
     email_sender.send_email(
         subject="Test Email",
         body="This is a test email.",
-        to_emails="sa.akhtar.official01@gmail.com",)
+        to_emails="add your email here",)
